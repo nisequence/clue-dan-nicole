d6da44b58e92249e6c616e4a4b9987852b93ebea
DanRHenry <danielrhenry@gmail.com>
2024-08-05 18:58:32 -0400

    base react framework done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
 import './App.css';
+import Gameboard from './components/gameboard/Gameboard'
+
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Gameboard/>}/>
+
+      </Routes>
     </div>
   );
 }
