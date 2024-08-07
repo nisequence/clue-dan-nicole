0db425db137a32e8e087e34b46374b72974fd3f7
DanRHenry <danielrhenry@gmail.com>
2024-08-07 15:23:15 -0400

    working on adding the game board react-style

--- client/src/components/gameboard_container/gameboard/Gameboard.jsx
+++ client/src/components/gameboard_container/gameboard/Gameboard.jsx
@@ -1,17 +1,26 @@
+// Resources:
+// https://dev.to/francodalessio/understanding-react-elements-and-jsx-4dc3 -- old
+// https://react.dev/reference/react-dom/client/createRoot
+
 import React from "react";
+import ReactDOM from "react-dom"
+import {createRoot} from 'react-dom/client'
 
 export default function Gameboard() {
   const gameBoard = () => {
-    const gameBoard = React.createElement("div")
+    const domNode = document.getElementById('root');
+
     // including the start squares, the board is 24 across by 25 vertically
     for (let horiz = 0; horiz < 24; horiz++) {
       for (let vert = 0; vert < 25; vert++) {
         // let square = document.createElement("div");
-        const square = React.createElement("div", {
+        const square = createRoot("div", {
           style: { height: "1.5em", width: "1.5em" },
           id: `game-tile-${vert}_${horiz}`,
           className: "gameSquares",
         });
+        // const gameBoard = React.createElement("div", null, square)
+        createRoot(square, square)
         // square.style.height = "1.5em";
         // square.style.width = "1.5em";
         // square.id = `game-tile-${vert}_${horiz}`;
@@ -22,5 +31,9 @@ export default function Gameboard() {
     }
     return gameBoard
   };
-  return <div>{gameBoard()}</div>;
+  return (
+  <div id="root">
+  <div>{gameBoard()}</div>;
+  </div>
+  )
 }
