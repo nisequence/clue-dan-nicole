faae5c399d34570f2c51413717c07f2948e1ab2c
DanRHenry <danielrhenry@gmail.com>
2024-08-07 13:51:02 -0400

    working on the game board

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import './App.css';
-import Gameboard from './components/gameboard/Gameboard'
-
+import Gameboard_Container from './components/gameboard_container/Gameboard_container';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Gameboard/>}/>
+        <Route path='/' element={<Gameboard_Container/>}/>
 
       </Routes>
     </div>
---
+++ client/src/components/gameboard_container/Gameboard_container.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Gameboard from "./gameboard/Gameboard";
+
+export default function Gameboard_Container() {
+
+  return (
+    <>
+      <div id="gameboard-container">
+        <Gameboard/>
+      </div>
+    </>
+  );
+}
