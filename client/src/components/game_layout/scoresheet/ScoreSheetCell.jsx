5a52cafdc3f5913b8983118c2fb7f70b9aeaac00
DanRHenry <danielrhenry@gmail.com>
2024-08-18 04:18:45 -0400

    switching to desktop

--- client/src/components/game_layout/scoresheet/ScoreSheetCell.jsx
+++ client/src/components/game_layout/scoresheet/ScoreSheetCell.jsx
@@ -1,75 +1,71 @@
 // replace this with something newer
 // https://legacy.reactjs.org/docs/forms.html
 
-import React, { useEffect, useState, useRef } from "react";
+import React, { useRef, useState } from "react";
 
 export default function ScoreSheetCell(props) {
   const [cellState, setCellState] = useState("unchecked");
-  const [cellText, setCellText] = useState("unchecked")
-  const cellContents = useRef();
+  const [cellText, setCellText] = useState(<div>start</div>);
 
-  function switchCellText () {
-    if (cellState === "unchecked") {
-      setCellState("checked")
-      setCellText(<div>checked</div>)
-    }
-    else if (cellState === "checked") {
-      setCellState("input")
-      setCellText(<form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.value} onChange={handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>)
-    } else if (cellState === "input") {
-      setCellState("unchecked")
-      setCellText(<div>unchecked</div>) 
-    }
+  const cellContent = useRef();
 
+  const checkCellText = () => {
+      if (cellState === "unchecked") {
+          setCellState("checked")
+          switchCellText()
+
+          console.log(cellState)
+        } else if (cellState === "checked") {
+            setCellState("input")
+            console.log(cellState)
+            switchCellText()
+        } else if (cellState === "input") {
+            handleSubmit()
+            // setCellState("unchecked")
+            console.log(cellState)
+        }
+    // }
+    
+    // else if (cellState.current === "input") {
+    //     switchCellText()
+    // } else if (cellState.current === "checked"){
+    //     switchCellText()
+    // } else if (cellState.current === "unchecked") {
+    //     switchCellText()
+    // }
   }
+
+  const switchCellText = () => {
+    // console.log(cellContent.current);
+    if (cellState === "unchecked") {
+      setCellState("checked");
+      cellContent.current = <div onClick={switchCellText}>checked</div>;
+      setCellText(cellContent.current);
+    } else if (cellState === "checked") {
+      setCellState("input");
+      cellContent.current = (
+        <form onSubmit={handleSubmit}>
+          <input type="text" onClick={null} onSubmit={handleSubmit} />
+        </form>
+      );
+      setCellText(cellContent.current);
+    } else if (cellState === "input") {
+      setCellState("unchecked");
+      cellContent.current = <div onClick={switchCellText}>unchecked</div>;
+      setCellText(cellContent.current);
+    }
+  };
 
   function handleSubmit() {
-    console.log("text submitted")
+    // e.preventDefault()
+    console.log("text submitted");
+    // switchCellText();
   }
 
-  function handleChange () {
-    console.log("handling change")
-  }
-
-  // useEffect(() => {
-  //   console.log("cellState:", cellState);
-  //   if (cellState === "clicked") {
-  //     console.log(cellState);
-  //     cellContents.current = (
-  //       <div
-  //         onClick={() => {
-  //           setCellState("clicked");
-  //           // props.setscoresheetContents(props.scoresheetContents)
-  //         }}
-  //       >
-  //         clicked
-  //       </div>
-  //     );
-  //   } 
-  //   if (cellState === "unclicked") {
-  //     console.log(cellState);
-
-  //     cellContents.current = (
-  //       <div
-  //         onClick={() => {
-  //           setCellState("clicked");
-  //           props.setscoresheetContents(props.scoresheetContents)
-  //         }}
-  //       >
-  //         unclicked
-  //       </div>
-  //     );
-  //     console.log("cellContents:", cellContents.current);
-  //   }
-  // }, [cellState]);
-
-  return <>
-  <div onClick={switchCellText} onSubmit={switchCellText}>{cellText}</div>
-  </>;
+  return (
+    // <>
+    //   <div onClick={switchCellText}>{cellText}</div>
+    // </>
+    <div onClick={checkCellText}>{cellText}</div>
+  );
 }
