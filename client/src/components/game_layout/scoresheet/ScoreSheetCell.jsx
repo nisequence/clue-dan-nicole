6c9a226eb092df83840f149a2ca81a05f3fbfaf1
DanRHenry <danielrhenry@gmail.com>
2024-08-19 09:42:56 -0400

    working on cell text

--- client/src/components/game_layout/scoresheet/ScoreSheetCell.jsx
+++ client/src/components/game_layout/scoresheet/ScoreSheetCell.jsx
@@ -2,30 +2,35 @@
 // https://legacy.reactjs.org/docs/forms.html
 
 import React, { useRef, useState } from "react";
-
+import { Form, FormGroup, Input } from "reactstrap";
 export default function ScoreSheetCell(props) {
   const [cellState, setCellState] = useState("unchecked");
   const [cellText, setCellText] = useState(<div>start</div>);
 
   const cellContent = useRef();
+  const formTextRef = useRef();
 
   const checkCellText = () => {
-      if (cellState === "unchecked") {
-          setCellState("checked")
-          switchCellText()
+    if (cellState === "unchecked") {
+      setCellState("checked");
+      switchCellText();
 
-          console.log(cellState)
-        } else if (cellState === "checked") {
-            setCellState("input")
-            console.log(cellState)
-            switchCellText()
-        } else if (cellState === "input") {
-            handleSubmit()
-            // setCellState("unchecked")
-            console.log(cellState)
-        }
+      console.log(cellState);
+    } else if (cellState === "checked") {
+      setCellState("input");
+      console.log(cellState);
+      switchCellText();
+    } else if (cellState === "input") {
+    //   handleSubmit();
+      // setCellState("unchecked")
+      console.log(cellState);
+    } else if (cellState === "manualText") {
+        // setCellState("manualText")
+        console.log(cellState);
+        switchCellText()
+    }
     // }
-    
+
     // else if (cellState.current === "input") {
     //     switchCellText()
     // } else if (cellState.current === "checked"){
@@ -33,7 +38,7 @@ export default function ScoreSheetCell(props) {
     // } else if (cellState.current === "unchecked") {
     //     switchCellText()
     // }
-  }
+  };
 
   const switchCellText = () => {
     // console.log(cellContent.current);
@@ -44,9 +49,21 @@ export default function ScoreSheetCell(props) {
     } else if (cellState === "checked") {
       setCellState("input");
       cellContent.current = (
-        <form onSubmit={handleSubmit}>
-          <input type="text" onClick={null} onSubmit={handleSubmit} />
-        </form>
+        <Form>
+          <FormGroup>
+            <Input
+              placeholder="Hello"
+              innerRef={formTextRef}
+              type="text"
+              autoComplete="on"
+              onSubmit={(e) => {
+                e.preventDefault()
+                alert(formTextRef)
+                // handleSubmit()
+              }} 
+            />
+          </FormGroup>
+        </Form>
       );
       setCellText(cellContent.current);
     } else if (cellState === "input") {
@@ -58,14 +75,31 @@ export default function ScoreSheetCell(props) {
 
   function handleSubmit() {
     // e.preventDefault()
+   
+        alert("formText:",formTextRef.current.value)
+        setCellState("manualText")
+        cellContent.current = <div>{formTextRef.current.value}</div>
+    
     console.log("text submitted");
     // switchCellText();
   }
+
+//   const outputDisplay = () => {
+//     // if (formText.current.value !== null) {
+//     //   return formText.current.value;
+//     // } else {
+//       return cellText;
+//     // }
+//   };
 
   return (
     // <>
     //   <div onClick={switchCellText}>{cellText}</div>
     // </>
-    <div onClick={checkCellText}>{cellText}</div>
+    <div onClick={(e) => {
+        e.preventDefault()
+        // alert("it's clicking time")
+        checkCellText()
+    }}>{cellText}</div>
   );
 }
