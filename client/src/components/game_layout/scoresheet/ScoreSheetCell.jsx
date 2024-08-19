d55c413fe844131e3e40ea7b6107b6dcbf30a342
DanRHenry <danielrhenry@gmail.com>
2024-08-19 11:34:51 -0400

    forms working well

--- client/src/components/game_layout/scoresheet/ScoreSheetCell.jsx
+++ client/src/components/game_layout/scoresheet/ScoreSheetCell.jsx
@@ -2,104 +2,52 @@
 // https://legacy.reactjs.org/docs/forms.html
 
 import React, { useRef, useState } from "react";
-import { Form, FormGroup, Input } from "reactstrap";
+import ScoresheetInputForm from "./ScoresheetInputForm";
 export default function ScoreSheetCell(props) {
-  const [cellState, setCellState] = useState("unchecked");
-  const [cellText, setCellText] = useState(<div>start</div>);
-
   const cellContent = useRef();
-  const formTextRef = useRef();
 
-  const checkCellText = () => {
+  const [cellState, setCellState] = useState("unchecked");
+  const [cellText, setCellText] = useState(<div className="scoresheetCell" ><div style={{visibility: "hidden"}}>unchecked</div></div>);
+
+  const checkCellState = () => {
     if (cellState === "unchecked") {
       setCellState("checked");
-      switchCellText();
-
-      console.log(cellState);
-    } else if (cellState === "checked") {
-      setCellState("input");
-      console.log(cellState);
-      switchCellText();
-    } else if (cellState === "input") {
-    //   handleSubmit();
-      // setCellState("unchecked")
-      console.log(cellState);
-    } else if (cellState === "manualText") {
-        // setCellState("manualText")
-        console.log(cellState);
-        switchCellText()
+      switchCellText("checked");
     }
-    // }
-
-    // else if (cellState.current === "input") {
-    //     switchCellText()
-    // } else if (cellState.current === "checked"){
-    //     switchCellText()
-    // } else if (cellState.current === "unchecked") {
-    //     switchCellText()
-    // }
-  };
-
-  const switchCellText = () => {
-    // console.log(cellContent.current);
-    if (cellState === "unchecked") {
-      setCellState("checked");
-      cellContent.current = <div onClick={switchCellText}>checked</div>;
-      setCellText(cellContent.current);
-    } else if (cellState === "checked") {
+    if (cellState === "checked") {
       setCellState("input");
-      cellContent.current = (
-        <Form>
-          <FormGroup>
-            <Input
-              placeholder="Hello"
-              innerRef={formTextRef}
-              type="text"
-              autoComplete="on"
-              onSubmit={(e) => {
-                e.preventDefault()
-                alert(formTextRef)
-                // handleSubmit()
-              }} 
-            />
-          </FormGroup>
-        </Form>
-      );
-      setCellText(cellContent.current);
-    } else if (cellState === "input") {
-      setCellState("unchecked");
-      cellContent.current = <div onClick={switchCellText}>unchecked</div>;
-      setCellText(cellContent.current);
+      switchCellText("input");
+    }
+    if (cellState === "manualText") {
+      setCellState("unchecked")
+      switchCellText("unchecked");
     }
   };
 
-  function handleSubmit() {
-    // e.preventDefault()
-   
-        alert("formText:",formTextRef.current.value)
-        setCellState("manualText")
-        cellContent.current = <div>{formTextRef.current.value}</div>
-    
-    console.log("text submitted");
-    // switchCellText();
-  }
-
-//   const outputDisplay = () => {
-//     // if (formText.current.value !== null) {
-//     //   return formText.current.value;
-//     // } else {
-//       return cellText;
-//     // }
-//   };
+  const switchCellText = (cellState) => {
+    if (cellState === "checked") {
+      setCellText(<div className="scoresheetCell"><div onClick={switchCellText}>checked</div></div>);
+    }
+    if (cellState === "unchecked") {
+      setCellText(<div className="scoresheetCell"><div style={{visibility: "hidden"}} onClick={switchCellText}>unchecked</div></div>)
+    }
+    if (cellState === "input") {
+      setCellText(<ScoresheetInputForm 
+        setCellText={setCellText}
+        switchCellText = {switchCellText}
+        setCellState= {setCellState}
+        />);
+    }
+  };
 
   return (
-    // <>
-    //   <div onClick={switchCellText}>{cellText}</div>
-    // </>
-    <div onClick={(e) => {
-        e.preventDefault()
-        // alert("it's clicking time")
-        checkCellText()
-    }}>{cellText}</div>
+    <div
+      onClick={(e) => {
+        e.preventDefault();
+        checkCellState();
+      }}
+    >
+      {cellText}
+    </div>
   );
 }
