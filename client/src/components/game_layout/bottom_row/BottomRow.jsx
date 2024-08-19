274db049e319379fdefb1447db7e5f9d6fcee69a
nisequence <nicolesekol@gmail.com>
2024-08-19 13:37:17 -0400

    minor styling changes - NS

--- client/src/components/game_layout/bottom_row/BottomRow.jsx
+++ client/src/components/game_layout/bottom_row/BottomRow.jsx
@@ -4,7 +4,18 @@ import HeldCards from "./held_cards/HeldCards";
 export default function BottomRow() {
   return (
     <>
-      <div id="bottomRow">
+      <div
+        id="bottomRow"
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          // alignContent: "center",
+          justifyContent: "center",
+          // alignItems: "center",
+          // alignSelf: "center",
+          backgroundColor: "blue",
+        }}
+      >
         BottomRow
         <HeldCards />
       </div>
